30679f570e9dd26defe9f670be0a8c765f4398b8
Jeevan HR <jeevanhr7@gmail.com>
2016-01-26 00:27:27 +0530

    Added gulp task runner and added dev dependencies to package.json

---
+++ Gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+/* https://github.com/angular-fullstack/generator-angular-fullstack
+ * You can use this simplified gulpfile for creating dist by $>`gulp build`
+ * Deploy to heroku or dokku using git or heroku toolbelt
+ */
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var os = require('os');
+var gulp = require('gulp');
+var open = require('gulp-open');
+var nodemon = require('gulp-nodemon');
+
+
+// This is a simple task to run node.js app and restart when code edited by developers
+gulp.task('run', function () {
+    nodemon({ script: 'app.js'
+        , ext: 'html js'
+        , ignore: [] //'ignored.js'
+        , tasks: [] })
+        .on('restart', function () {
+            console.log('restarted!')
+        })
+})
+
+
+// Run App - This task is to open index.html
+gulp.task('open', function(){
+    gulp.src('./index.html')
+        .pipe(open());
+});
